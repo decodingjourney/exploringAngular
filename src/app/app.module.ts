17a0605c1bec3c22d8a54c42eb263d6e70a68195
decodingjourney <vikramanand.jha@gmail.com>
2019-03-04 17:10:23 +0530

    Startinf from scrach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    ServerComponent
   ],
   imports: [
     BrowserModule
